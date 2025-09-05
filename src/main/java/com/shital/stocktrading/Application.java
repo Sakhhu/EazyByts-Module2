f07f342f1e90e5883eed32ffeb41c371d28e0a03
shital <shitalsavant89@gmail.com>
2025-09-05 21:40:17 +0530

    Initial commit

--- src/main/java/com/shital/stocktrading/Application.java
+++ src/main/java/com/shital/stocktrading/Application.java
@@ -2,12 +2,15 @@ package com.shital.stocktrading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableJpaRepositories(basePackages = "com.stocktrading.repository")
+@EntityScan(basePackages = "com.stocktrading.model")
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.shital.stocktrading", "com.stocktrading"})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
